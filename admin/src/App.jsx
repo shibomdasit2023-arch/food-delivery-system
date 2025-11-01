de94527709910ad24494b3d612c8b85e55591fe0
shibomdasit2023-arch <shibomdas.it2023@nsec.ac.in>
2025-11-01 09:45:18 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
-import List from './pages/List/list'
+import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
  import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
