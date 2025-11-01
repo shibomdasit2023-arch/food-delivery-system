8a744242c8151fc30a2d5f84a43537ab255ec08a
shibomdasit2023-arch <shibomdas.it2023@nsec.ac.in>
2025-11-01 08:59:31 +0530

    Fix: completely remove submodule tracking for admin and frontend

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-letf">
+            <img src={assets.logo} alt="" />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolore beatae in provident esse illo. Vitae maiores voluptatem iste. Ut ea qui illo quasi eligendi doloribus nulla perferendis! Autem, ad.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>Company</h2>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+        </div>
+        <div className="footer-content-right">
+            <h2>Get In touch</h2>
+            <ul>
+                <li>+7693379468</li>
+                <li>mhjbecsb@gmail.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="foooter-copyright">Copyeryeutbtknvtk,ev rvlr s</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your fav food ere</h2>
+            <p>bvsrtjmkbvjrttscssemjncysebycstenc</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
